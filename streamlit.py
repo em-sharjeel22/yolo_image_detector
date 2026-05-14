fa102529b65aa6b306d225dca9bcdebf5d64c74c
em-sharjeel22 <xxsharjeelkhan2207@gmail.com>
2026-05-14 19:27:39 +0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,7 @@ st.title("YOLO Object Detection")
 
 uploaded_file = st.file_uploader("Upload Image")
 
-if uploaded_file:
+if uploaded_file is not None:
 
     # image = Image.open(uploaded_file)
     try:
